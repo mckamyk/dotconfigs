4e0c0e47293c8d661a9350b00a78069f32a1ae79
McKamyk <mckittrick.kaminski@gmail.com>
2024-12-30 23:08:53 -0600

    fix eslint when config doesn't exist

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,33 +1,47 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		local utils = require("null-ls.utils")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        require("none-ls.diagnostics.eslint_d"),
-        null_ls.builtins.formatting.prettier,
-      },
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.stylua,
+				require("none-ls.diagnostics.eslint_d").with({
+					filetypes = { "javascript", "typescript", "javascriptreact", "typescriptreact" },
+					condition = function()
+						return null_ls.utils.root_has_file({
+							"eslint.config.js",
+							".eslintrc",
+							".eslintrc.js",
+							".eslintrc.cjs",
+							".eslintrc.yaml",
+							".eslintrc.yml",
+							".eslintrc.json",
+						})
+					end,
+				}),
+			},
 
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ async = false })
-            end,
-          })
-        end
-      end,
-    })
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end
+			end,
+		})
 
-    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+	end,
 }
