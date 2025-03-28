9c5bfe9653c345a2236dcbfb9805dc51a6a58bb7
McKamyk <mckittrick.kaminski@gmail.com>
2025-03-28 13:59:10 -0500

    dark/light toggle for ghostty and nvim

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,9 +1,24 @@
 return {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  config = function ()
-    vim.cmd.colorscheme "catppuccin-mocha"
-  end
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
+  },
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      set_dark_mode = function()
+        vim.cmd.colorscheme("catppuccin-mocha")
+      end,
+      set_light_mode = function()
+        vim.cmd.colorscheme("catppuccin-latte")
+      end,
+      update_interval = 3000,
+      fallback = "dark",
+    },
+  },
 }
