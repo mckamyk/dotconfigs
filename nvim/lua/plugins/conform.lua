bbc0379fd0fc611d8a89dbcf19c5c6c60c95338d
McKamyk <mckittrick.kaminski@gmail.com>
2025-09-15 22:56:14 -0500

    vtsls

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,21 +1,64 @@
 return {
   'stevearc/conform.nvim',
   event = { "BufReadPre", "BufNewFile" },
-  opts = {
+  opts = function()
+    local function has_file(patterns)
+      for _, pattern in ipairs(patterns) do
+        if vim.fn.glob(pattern) ~= "" then
+          return true
+        end
+      end
+      return false
+    end
 
-    formatters_by_ft = {
-      javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
-      typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
-      javascriptreact = { "biome-check", "biome", "prettierd", "prettier", stop_after_first = true },
-      typescriptreact = { "biome-check", "biome", "prettierd", "prettier", stop_after_first = true },
-    },
-    formatters = {
-      biome = {
-        require_cwd = true
+    local hasEslint = has_file({
+      "eslint.config.js",
+      ".eslintrc",
+      ".eslintrc.js",
+      ".eslintrc.cjs",
+      ".eslintrc.yaml",
+      ".eslintrc.yml",
+      ".eslintrc.json",
+    })
+
+    local hasPrettier = has_file({
+      ".prettierrc",
+      ".prettierrc.json",
+      ".prettierrc.yml",
+      ".prettierrc.yaml",
+      ".prettierrc.js",
+      ".prettierrc.cjs",
+      "prettier.config.js",
+      "prettier.config.cjs",
+    })
+
+    local formatters_by_ft
+    if hasEslint or hasPrettier then
+      formatters_by_ft = {
+        javascript = { "prettierd", "prettier" },
+        typescript = { "prettierd", "prettier" },
+        javascriptreact = { "prettierd", "prettier" },
+        typescriptreact = { "prettierd", "prettier" },
+      }
+    else
+      formatters_by_ft = {
+        javascript = { "biome" },
+        typescript = { "biome" },
+        javascriptreact = { "biome" },
+        typescriptreact = { "biome" },
+      }
+    end
+
+    return {
+      formatters_by_ft = formatters_by_ft,
+      formatters = {
+        biome = {
+          require_cwd = true
+        },
       },
-    },
-    format_on_save = {
-      lsp_fallback = true
+      format_on_save = {
+        lsp_fallback = true
+      }
     }
-  }
+  end
 }
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ts_ls", "tailwindcss", "solidity" },
+				ensure_installed = { "lua_ls", "vtsls", "tailwindcss", "solidity", "biome" },
 			})
 		end,
 	},
@@ -18,11 +18,41 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+			local function has_file(patterns)
+				for _, pattern in ipairs(patterns) do
+					if vim.fn.glob(pattern) ~= "" then
+						return true
+					end
+				end
+				return false
+			end
+
+			local hasEslint = has_file({
+				"eslint.config.js",
+				".eslintrc",
+				".eslintrc.js",
+				".eslintrc.cjs",
+				".eslintrc.yaml",
+				".eslintrc.yml",
+				".eslintrc.json",
+			})
+
+			local hasPrettier = has_file({
+				".prettierrc",
+				".prettierrc.json",
+				".prettierrc.yml",
+				".prettierrc.yaml",
+				".prettierrc.js",
+				".prettierrc.cjs",
+				"prettier.config.js",
+				"prettier.config.cjs",
+			})
+
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
-			lspconfig.ts_ls.setup({
+			lspconfig.vtsls.setup({
 				capabilities = capabilities,
 			})
 			lspconfig.solidity.setup({
@@ -31,6 +61,12 @@ return {
 			lspconfig.gopls.setup({
 				capabilities = capabilities,
 			})
+
+			if not hasEslint and not hasPrettier then
+				lspconfig.biome.setup({
+					capabilities = capabilities,
+				})
+			end
 
 			vim.diagnostic.config({
 				signs = {
@@ -55,6 +91,7 @@ return {
 
 			vim.keymap.set("n", "<leader>n", vim.diagnostic.goto_next, {})
 			vim.keymap.set("n", "<leader>m", vim.diagnostic.goto_prev, {})
+			vim.keymap.set("n", "<leader>li", "<cmd>LspInfo<CR>", {})
 		end,
 	},
 }
